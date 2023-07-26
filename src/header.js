c21f6af017a52e93a08e90f1cb78ae69adf166b7
jmy-k <myjuliakan@gmail.com>
2023-07-26 20:22:21 +0800

    added basic styling so it looks like "tabs"
    i'm still really bad at cropping images.

--- src/header.js
+++ src/header.js
@@ -2,30 +2,38 @@ const headBannerFunction = () => {
     const headBanner = document.createElement('div');
     headBanner.id="head-banner";
 
+    const bannerImgContainer = document.createElement('div');
+    bannerImgContainer.id="banner-img-container"
     const bannerImg = document.createElement('img');
     bannerImg.src="../src/banner.jpg";
     bannerImg.id="banner-image"
-    headBanner.appendChild(bannerImg);
+    bannerImgContainer.appendChild(bannerImg);
+    headBanner.appendChild(bannerImgContainer);
 
     const headerTitle=document.createElement('div');
     headerTitle.id="restaurant-title";
     headerTitle.textContent="Chez Julia";
     headBanner.appendChild(headerTitle);
 
+    const tabTitles = document.createElement('div');
+    tabTitles.id="tab-titles"
+
     const homeTitle=document.createElement('div');
     homeTitle.id="home-title";
     homeTitle.textContent="Home";
-    headBanner.appendChild(homeTitle);
+    tabTitles.appendChild(homeTitle);
 
     const menuTitle=document.createElement('div');
     menuTitle.id="menu-title";
     menuTitle.textContent="Menu";
-    headBanner.append(menuTitle);
+    tabTitles.append(menuTitle);
 
     const contactTitle=document.createElement('div');
     contactTitle.id="contact-title";
     contactTitle.textContent="Contact";
-    headBanner.append(contactTitle);
+    tabTitles.append(contactTitle);
+
+    headBanner.appendChild(tabTitles);
 
     return headBanner;
 } 
